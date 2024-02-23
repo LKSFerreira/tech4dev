3bfcc5295ef340e4a89a72133aacd94420991154
Thiago Souza <thiagosouza.contato01@gmail.com>
2024-02-23 13:45:48 -0300

    :dizzy: Adicionando animações JS
    Adicionei um efeito de reveal à página index com Javascript e CSS.

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal("#title", {
+  origin: "top",
+  duration: 2000,
+  distance: "40%",
+  reset: true,
+});
+
+ScrollReveal().reveal("#social_media ul li", {
+  origin: "left",
+  duration: 2000,
+  distance: "70%",
+  reset: true,
+});
