a44b9e4535daa7846dc3384aa93ac433e3f158c4
Lucas Ferreira <lks_ferreira@hotmail.com>
2024-02-27 20:12:40 -0300

    :recycle: Refatorado nome da classe do formulário

--- scripts/contato.js
+++ scripts/contato.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
   $("#whatsapp").mask("(00) 00000-0000");
 });
+
+
+ScrollReveal().reveal("#title", {
+  origin: "top",
+  duration: 2000,
+  distance: "40%",
+  reset: true,
+});
+
+// ScrollReveal().reveal("#social_media ul li", {
+//   origin: "left",
+//   duration: 2000,
+//   distance: "70%",
+//   reset: true,
+// });
