aa8f2b722267b13530d9a681d339b590e45e8e47
Thiago Souza <thiagosouza.contato01@gmail.com>
2024-04-01 13:56:02 -0300

    :iphone: Feito a parte responsiva da Index
    Realizada a parte responsiva da página index, foram adicionados dois botões para abrir e fechar a sidebar. Pequenas correções no html, e estilização no css com as Medias Queries. Também foi adicionado o Javascript para a sidebar

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,28 @@
-// ScrollReveal().reveal("#title", {
-//   origin: "top",
-//   duration: 2000,
-//   distance: "40%",
-//   reset: true,
-// });
+// Seleção de elementos ⬇️
+const open_menu = document.querySelector("#openMenu");
+const close_button = document.querySelector("#closeMenu");
+const navigation = document.querySelector("#navigation");
 
-// ScrollReveal().reveal("#social_media ul li", {
-//   origin: "left",
-//   duration: 2000,
-//   distance: "70%",
-//   reset: true,
-// });
+// Eventos ⬇️
+open_menu.addEventListener("click", () => {
+  navigation.style.transform = "translate(0)";
+});
+
+close_button.addEventListener("click", () => {
+  navigation.style.transform = "translate(100%)";
+});
+
+// ScrollReveal para animações ⬇️
+ScrollReveal().reveal("#title", {
+  origin: "top",
+  duration: 2000,
+  distance: "40%",
+  reset: true,
+});
+
+ScrollReveal().reveal("#social_media ul li", {
+  origin: "left",
+  duration: 2000,
+  distance: "70%",
+  reset: true,
+});
