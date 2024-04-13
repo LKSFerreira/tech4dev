c11e0fcfc756fec450fd84956df361b7ee9a3bd7
Lucas Ferreira <lks_ferreira@hotmail.com>
2024-04-13 01:13:26 -0300

    :recycle: Renomeado data `developers.json` para `devs.json`

--- scripts/devs.js
+++ scripts/devs.js
@@ -1,18 +1,18 @@
 import { getGitHubUser } from "../services/apiGitHub.js";
 
-const apiUrl = "../data/developers.json";
+const apiUrl = "../data/devs.json";
 
-const headers = {
-  "X-Master-Key":
-    "$2a$10$PPyqs.BlmV6IA0Ih5ZG4WetjD0k/p380F918wD8Fwj8rE1WVwH.7m",
-  "X-Access-Key":
-    "$2a$10$YEgyJrRsyJBtWbP/o5xvHOw25iMJ0Di853oA7RayZz65OmBCvnsZi",
-};
+// const headers = {
+//   "X-Master-Key":
+//     "$2a$10$PPyqs.BlmV6IA0Ih5ZG4WetjD0k/p380F918wD8Fwj8rE1WVwH.7m",
+//   "X-Access-Key":
+//     "$2a$10$YEgyJrRsyJBtWbP/o5xvHOw25iMJ0Di853oA7RayZz65OmBCvnsZi",
+// };
 
-const options = {
-  method: "GET",
-  headers: headers,
-};
+// const options = {
+//   method: "GET",
+//   headers: headers,
+// };
 
 window.addEventListener("DOMContentLoaded", function () {
   const containerDevs = document.querySelector(".container-devs");
